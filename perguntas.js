d02c36902ecb2d1c7ffffd56f2acbbc3bb59c228
rfernandes777 <rafael.laval@escola.pr.gov.br>
2025-10-10 08:02:36 -0300

    ehbjwfeh

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'programaçao'
+    'oque e python?;
+    'o python e uma linguagem de proglamaçao
+    criarCartao(
+        'geografia;
+        'Qual a capital da frança;
+        'A capital da frança e paris'
+    )
+    criarCartao(
+        'proglamaçao;
+        'Oque e uma funçao?;
+        'Uma funçao e um bloco de codigo que executa alguma tarefa'
+    )
+)
